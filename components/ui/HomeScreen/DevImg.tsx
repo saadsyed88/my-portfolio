c37450071f28c12f7d24372d15704277ab0bed0d
saadsyed88 <saad.shah@systemsltd.com>
2024-02-14 18:45:46 +0500

    Hero Section Done

---
+++ components/ui/HomeScreen/DevImg.tsx
@@ -0,0 +1,16 @@
+import Image from 'next/image';
+
+interface IDevImgProps {
+  containerStyles?: string;
+  imgSrc?: string;
+}
+
+const DevImg = ({ containerStyles, imgSrc }: IDevImgProps) => {
+  return (
+    <div className={`${containerStyles}`}>
+      <Image src={imgSrc!} fill priority alt='' />
+    </div>
+  );
+};
+
+export default DevImg;
---
+++ components/ui/shared/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <h1 className='text-xl'>Footer</h1>
+    </footer>
+  );
+};
+
+export default Footer;
