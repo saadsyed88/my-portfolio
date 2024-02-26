8cb72fd7b104980f9600e1cc7ab6493859cfcd67
saadsyed88 <saad.shah@systemsltd.com>
2024-02-26 18:41:14 +0500

    git ignore updated (2)

---
+++ next-env.d.ts
@@ -0,0 +1,6 @@
+/// <reference types="next" />
+/// <reference types="next/image-types/global" />
+
+// NOTE: This file should not be edited
+// see https://nextjs.org/docs/basic-features/typescript for more information.
+VERCEL_FORCE_NO_BUILD_CACHE = 1
