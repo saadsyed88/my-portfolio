341427e7a60a2749efae6c6b5fa7cba94ea3ec74
saadsyed88 <saad.shah@systemsltd.com>
2024-02-26 17:38:50 +0500

    error fixed

--- components/ui/Login/LoginForm.tsx
+++ components/ui/Login/LoginForm.tsx
@@ -1,5 +1,4 @@
 import { cookies } from 'next/headers';
-import { createClient } from '@/utils/supabase/server';
 import { redirect } from 'next/navigation';
 import { useState } from 'react';
 import { Button } from '../button';
@@ -9,35 +8,15 @@ export default async function LoginForm({
 }: {
   searchParams: { message: string };
 }) {
-  const cookieStore = cookies();
-  const supabase = createClient(cookieStore);
-  const {
-    data: { user },
-  } = await supabase.auth.getUser();
-
   const signIn = async (formData: FormData) => {
     'use server';
 
     const email = formData.get('email') as string;
     const password = formData.get('password') as string;
     const cookieStore = cookies();
-    const supabase = createClient(cookieStore);
-
-    const { error } = await supabase.auth.signInWithPassword({
-      email,
-      password,
-    });
-
-    if (error) {
-      return redirect('/login?message=Could not authenticate user');
-    }
 
     return redirect('/admin');
   };
-
-  if (user) {
-    return redirect('/admin');
-  }
 
   return (
     <>
